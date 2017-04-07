b14ec23b0e554a10a1006a8919b6cb6da3da620f
Chunxiao523 <cz31@rice.edu>
2017-04-06 20:37:17 -0500

    fdjskal

---
+++ lab3.c
@@ -0,0 +1,21 @@
+/*
+* Program for COMP 521 lab3
+* Author: Chunxiao Zhang, Xiangning Qi
+*/
+
+/*
+ * Start small, and add features and test as you go.
+ * For example, the first thing you need to be able to do is to start the server
+ * and have it read the disk to build its internal list of free blocks and free inodes.
+ * So start with that.  Then, maybe try Open (you can Open("/"), for example).
+ * Once you have Open, try Link (it's pretty simple).
+ * Then maybe add Read.  Just keep adding features one at a time, testing as you go.
+
+ */
+#include <comp421/filesystem.h>
+#include <comp421/iolib.h>
+#include <stdio.h>
+int main() {
+	return 0;
+	
+} 
